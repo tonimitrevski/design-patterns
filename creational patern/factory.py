a11d0284af9a360b3eee83a1b4c93107efc401d1
Toni Mitrevski <toni@stativa.com.mk>
2018-07-22 22:29:33 +0200

    creational patern

---
+++ creational patern/factory.py
@@ -0,0 +1,33 @@
+class Dog:
+    def __init__(self, name):
+    	self._name = name
+
+    def speak(self):
+		return "Woof!"
+
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Meow!"
+
+
+def get_pet(pet="dog"):
+    pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+    return pets[pet]
+
+
+d = get_pet("dog")
+
+print(d.speak())
+
+c = get_pet("cat")
+
+print(c.speak())
+
+
+
+
+
+
